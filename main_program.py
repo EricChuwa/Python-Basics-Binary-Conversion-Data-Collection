04bf7e4917673e0bbc91fea8d626d80e2282c811
EricChuwa <eric.c.chuwa@gmail.com>
2025-10-27 20:47:27 +0200

    Clean up

--- main_program.py
+++ main_program.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+# Importing functions
 from helper_functions import validate_input, convert_to_binary, create_message
 from file_manager import save_message, read_message
 from greetings import show_intro, show_exit_message
 
+# Funnction definition
 def get_user_info():
     Valid = False
     while True: # This Whil Loop is for name 
@@ -22,13 +25,23 @@ def get_user_info():
             return user_name, user_age
 
 
+# Main Code Block
 if __name__ == "__main__":
+     # Intro Text
      show_intro()
-     inputs=get_user_info()
+
+     # Taking user input and storing it
+     inputs=get_user_info() # input[0] = name; input[1] = age
+
+     # Converting into binary
      name_bin=convert_to_binary(inputs[0])
      age_bin=convert_to_binary(inputs[1])
+
+     # Handling the message
      message=create_message(inputs[0], inputs[1], name_bin, age_bin)
      message_file="user_message.txt"
      save_message(message)
      read_message(message_file)
+
+    # Printing exit message
      show_exit_message()
