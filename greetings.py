00d5d113a165c61af9b9637e73a9f11bcfe76d92
raphaelmstudios <r.musau@alustudent.com>
2025-10-27 16:24:54 +0200

    greetings

---
+++ greetings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def show_intro():
+     print("==============================")
+    print("Welcome to the User Info Program")
+    print("==============================")
+    print()
+
+def show_exit_message():
+    print()
+    print("Thank you for using the program. Goodbye!")
+    print()
